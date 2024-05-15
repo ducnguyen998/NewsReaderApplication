546654f7ea570dede01b7a924de35bb7662ab02a
ducnguyen.998 <duc18041998@gmail.com>
2024-05-16 00:15:54 +0700

    Setup layout Top stories, Sections

--- NewsReaderSystem/UI/Bars/NavigationBarViewmodel.cs
+++ NewsReaderSystem/UI/Bars/NavigationBarViewmodel.cs
@@ -1,13 +1,48 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using NewsReaderSystem.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace NewsReaderSystem.UI.Bars
 {
     public class NavigationBarViewmodel : ObservableObject
     {
+        public ICommand NavigateTopStoriesCommand { get; set; }
+        public ICommand NavigateSectionsCommand { get; set; }
+
+        public NavigationBarViewmodel(IServiceProvider serviceProvider)
+        {
+            this.NavigateTopStoriesCommand 
+                = new NavigateCommand(serviceProvider.CreateSingleViewNavigationService(Defines.EView.TopStories));
+            this.NavigateSectionsCommand 
+                = new NavigateCommand(serviceProvider.CreateSingleViewNavigationService(Defines.EView.Sections));
+        }
+    }
+
+    public class NavigateCommand : ICommand
+    {
+        private readonly INavigationService navigationService;
+
+        public NavigateCommand(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            navigationService.Navigate();
+        }
     }
 }
